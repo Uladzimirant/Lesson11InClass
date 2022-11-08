90e6531d7d60068ed26cc8306cb9dd3638a9f56b
Uladzimirant <vladimir.bwmail@gmail.com>
2022-11-08 21:02:40 +0300

    Добавьте файлы проекта.

---
+++ Lesson11InClass/PingPongPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson11InClass
+{
+    public class PingPongPlayer
+    {
+        private string _name;
+        event EventHandler<int>? PingPongEvent;
+
+        public PingPongPlayer(string name)
+        {
+            _name = name;
+        }
+
+        public void Play(object? sender, int amount)
+        {
+            Console.WriteLine(_name + " recieved " + sender);
+            Thread.Sleep(100);
+            PingPongEvent?.Invoke(this, amount + 1);
+            if (amount < 100) (sender as PingPongPlayer)?.PingPongEvent?.Invoke(this, amount + 1);
+        }
+
+        public void RegisterWith(PingPongPlayer p)
+        {
+            p.PingPongEvent += Play;
+        }
+        public void BeginPlay()
+        {
+            PingPongEvent?.Invoke(this, 0);
+        }
+
+        public override string ToString()
+        {
+            return _name;
+        }
+    }
+}
---
+++ Lesson11InClass/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Lesson11InClass
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            PingPongPlayer ping = new PingPongPlayer("Ping");
+            PingPongPlayer pong = new PingPongPlayer("Pong");
+            ping.RegisterWith(pong);
+            pong.RegisterWith(ping);
+            ping.BeginPlay();
+        }
+    }
+}
